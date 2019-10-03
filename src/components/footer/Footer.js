3eefd01e1664bff0e49631ab06cbc2271b5ac69c
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-10-04 01:35:12 +0300

    made overall improvments

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,29 @@
 import React from "react";
 import "./Footer.css";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import { blue } from "@material-ui/core/colors";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: "center",
+    background: "#3f51b5",
+    color: "white"
+  }
+}));
 
 const Footer = props => {
+  const classes = useStyles();
   return (
     <div className="shadow-5 footer white ba bw1 b--black">
-      <p className="f4">Made By Omri Gal Shenhav</p>
-      <p className="f4">2019</p>
+      <Paper className={classes.paper}>
+        <p className="f4">Made By Omri Gal Shenhav</p>
+        <p className="f4">2019</p>
+      </Paper>
     </div>
   );
 };
--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,41 +1,48 @@
 import React from "react";
 import "./Landing.css";
 import avatar from "../../assets/images/myAvatar.png";
-import { SocialIcon } from "react-social-icons";
+// import { SocialIcon } from "react-social-icons";
+
+const myLinks = [
+  {
+    name: "email",
+    url: "mailto:shenhav.omri@gmail.com",
+    icon: "fa fa-envelope-o"
+  },
+  {
+    name: "github",
+    url: "https://github.com/OmriGalShen",
+    icon: "fa fa-github"
+  },
+  {
+    name: "linkedin",
+    url: "https://www.linkedin.com/in/omri-gal-shenhav-95235232/",
+    icon: "fa fa-linkedin-square"
+  },
+  {
+    name: "whatsapp",
+    url: "https://wa.me/972543926292",
+    icon: "fa fa-whatsapp"
+  }
+];
 
 const Landing = props => {
   return (
     <div className="landing">
       <div className="banner">
         <img alt="avatar" src={avatar} className="avatar" />
-        <h1 className="f1 b white">Full Stuck Web Developer</h1>
+        <h1 className="f1 b white title">Full Stuck Web Developer</h1>
         <p className="white f3">Java | Python | Javascript | React | NodeJS</p>
         <div className="social-links">
-          <SocialIcon
-            url="mailto:shenhav.omri@gmail.com"
-            target="_blank"
-            className="social-link grow"
-            style={{ height: 200, width: 200 }}
-          />
-          <SocialIcon
-            url="https://github.com/OmriGalShen"
-            target="_blank"
-            className="social-link grow"
-            style={{ height: 200, width: 200 }}
-          />
-          <SocialIcon
-            url="https://www.linkedin.com/in/omri-gal-shenhav-95235232/"
-            target="_blank"
-            className="social-link grow"
-            style={{ height: 200, width: 200 }}
-          />
-          <SocialIcon
-            network="whatsapp"
-            target="_blank"
-            url="https://wa.me/972543926292"
-            className="social-link grow"
-            style={{ height: 200, width: 200 }}
-          />
+          {myLinks.map((link, index) => {
+            return (
+              <div key={index}>
+                <a href={link.url} target="_blank" rel="noopener noreferrer">
+                  <i className={link.icon} aria-hidden="true"></i>
+                </a>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
