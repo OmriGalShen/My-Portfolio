336cbfc6e53555019585abb892d468593dd07d66
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-10-05 02:27:10 +0300

    working on projects display

---
+++ src/components/projects/MediaCard.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345
+  },
+  media: {
+    height: 140
+  }
+});
+
+export default function MediaCard({ cardInfo }) {
+  const classes = useStyles();
+
+  let demoButton = null;
+  if (cardInfo.demoLink) {
+    demoButton = (
+      <Button size="small" color="primary">
+        <a
+          href={cardInfo.demoLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: "none" }}
+        >
+          Demo
+        </a>
+      </Button>
+    );
+  }
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={cardInfo.image}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {cardInfo.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {cardInfo.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          <a
+            href={cardInfo.projectLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none" }}
+          >
+            View Source
+          </a>
+        </Button>
+        {demoButton}
+      </CardActions>
+    </Card>
+  );
+}
---
+++ src/components/projects/myProjects.js
@@ -0,0 +1,26 @@
+import img from "../../assets/images/myAvatar.png";
+
+export const projectList = [
+  {
+    image: img,
+    title: "myTitle",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut justo id sem ornare ultrices. Quisque et purus et mi feugiat ultrices. Morbi et ornare libero. Quisque tincidunt bibendum euismod. Aliquam eget odio nulla. Suspendisse ornare pharetra ante ut laoreet. In ac feugiat lorem, non laoreet sem. Ut tristique erat vel lacus luctus, eget luctus odio euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin dignissim fringilla nisi eget ultrices. Nunc odio urna, mattis vel felis eu, eleifend imperdiet ipsum. Morbi sit amet ex tempor, lacinia mauris id, accumsan leo.",
+    projectLink: "https://google.com",
+    demoLink: "https://google.com"
+  },
+  {
+    image: img,
+    title: "myTitle",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut justo id sem ornare ultrices. Quisque et purus et mi feugiat ultrices. Morbi et ornare libero. Quisque tincidunt bibendum euismod. Aliquam eget odio nulla. Suspendisse ornare pharetra ante ut laoreet. In ac feugiat lorem, non laoreet sem. Ut tristique erat vel lacus luctus, eget luctus odio euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin dignissim fringilla nisi eget ultrices. Nunc odio urna, mattis vel felis eu, eleifend imperdiet ipsum. Morbi sit amet ex tempor, lacinia mauris id, accumsan leo.",
+    projectLink: "https://google.com"
+  },
+  {
+    image: img,
+    title: "myTitle",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut justo id sem ornare ultrices. Quisque et purus et mi feugiat ultrices. Morbi et ornare libero. Quisque tincidunt bibendum euismod. Aliquam eget odio nulla. Suspendisse ornare pharetra ante ut laoreet. In ac feugiat lorem, non laoreet sem. Ut tristique erat vel lacus luctus, eget luctus odio euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin dignissim fringilla nisi eget ultrices. Nunc odio urna, mattis vel felis eu, eleifend imperdiet ipsum. Morbi sit amet ex tempor, lacinia mauris id, accumsan leo.",
+    projectLink: "https://google.com"
+  }
+];
