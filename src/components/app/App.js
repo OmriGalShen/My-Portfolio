1652871229890b096bcb311f5b41087ca428e577
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-10-05 23:39:43 +0300

    added grid to app components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,21 +8,34 @@ import Landing from "../landing/Landing";
 import Projects from "../projects/Projects";
 import Resume from "../resume/Resume";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Switch className="main-container">
-          <Route exact path="/" component={Landing} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/resume" component={Resume} />
-          <Route path="/projects" component={Projects} />
-        </Switch>
-      </Router>
-      <Footer />
+      <Grid container>
+        <Router>
+          <Grid item xs={12}>
+            <div className="navbar">
+              <Navbar />
+            </div>
+          </Grid>
+          <Grid item xs={12}>
+            <div className="main-container">
+              <Switch>
+                <Route exact path="/" component={Landing} />
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/resume" component={Resume} />
+                <Route path="/projects" component={Projects} />
+              </Switch>
+            </div>
+          </Grid>
+          <Grid item xs={12}>
+            <Footer />
+          </Grid>
+        </Router>
+      </Grid>
     </div>
   );
 }
