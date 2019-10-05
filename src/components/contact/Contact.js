a0232159a8230275c561164520d114582ed866a5
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-10-06 01:08:21 +0300

    major rework on contact page

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,62 +1,96 @@
 import React from "react";
 import "./Contact.css";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper
+  },
+  listText: {
+    fontSize: 25,
+    fontFamily: "Oxygen"
+  }
+}));
+
+const ProfileSection = () => {
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <h1 className="profile-title">Omri Gal Shenhav</h1>
+      </Grid>
+      <Grid item xs={12}>
+        <img
+          alt="profile"
+          src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+          className="profile-pic"
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <p className="profile-des">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </Grid>
+    </Grid>
+  );
+};
+
+const InfoSection = () => {
+  const classes = useStyles();
+  return (
+    <Grid container className="info">
+      <Grid item xs={12}>
+        <div className="info-title">
+          <h1>Contact Me</h1>
+          <hr />
+        </div>
+        <div className="info-contact">
+          <div className={classes.root}>
+            <List component="nav" aria-label="main mailbox folders">
+              <ListItem button>
+                <i className="fab fa-phone-squre" aria-hidden="true" />
+                <p>item 1</p>
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <DraftsIcon />
+                </ListItemIcon>
+                <p>item 2</p>
+              </ListItem>
+            </List>
+          </div>
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
 
 const Contact = props => {
   return (
     <div className="contact">
-      <h1>Contact</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut
-        justo id sem ornare ultrices. Quisque et purus et mi feugiat ultrices.
-        Morbi et ornare libero. Quisque tincidunt bibendum euismod. Aliquam eget
-        odio nulla. Suspendisse ornare pharetra ante ut laoreet. In ac feugiat
-        lorem, non laoreet sem. Ut tristique erat vel lacus luctus, eget luctus
-        odio euismod. Vestibulum ante ipsum primis in faucibus orci luctus et
-        ultrices posuere cubilia Curae; Proin dignissim fringilla nisi eget
-        ultrices. Nunc odio urna, mattis vel felis eu, eleifend imperdiet ipsum.
-        Morbi sit amet ex tempor, lacinia mauris id, accumsan leo. Etiam
-        facilisis egestas est vel euismod. Phasellus elit odio, mollis in
-        convallis eget, aliquet sit amet nulla. Nunc pellentesque tellus vel
-        dignissim sagittis. Duis scelerisque a mauris quis suscipit. Integer
-        mollis diam eget hendrerit tristique. Cras id congue turpis. Proin
-        faucibus magna id accumsan tincidunt. Mauris in mauris eu felis varius
-        sodales. Pellentesque vel diam sollicitudin, ornare enim et, blandit
-        lacus. Integer accumsan scelerisque diam, sed maximus ligula condimentum
-        non. Vestibulum placerat sodales congue. Etiam tincidunt lectus velit,
-        ut efficitur enim mollis sit amet. Cras a lorem eget arcu pellentesque
-        ultrices. Nam at lacus ut dui sollicitudin bibendum. Vestibulum eget
-        accumsan orci. Etiam aliquet id ex sit amet posuere. Cras dapibus
-        sollicitudin neque, nec convallis quam tempor vel. Donec lectus tellus,
-        vehicula ac felis ut, blandit volutpat odio. Vivamus porta, neque sed
-        accumsan fermentum, urna erat rutrum elit, in convallis metus dolor id
-        nulla. In ullamcorper massa non nisl sodales, sit amet laoreet urna
-        tristique. Vestibulum ante ipsum primis in faucibus orci luctus et
-        ultrices posuere cubilia Curae; Sed mattis dapibus orci, eu ornare risus
-        sollicitudin eget. Mauris semper bibendum hendrerit. Ut tempus lorem
-        nunc, ut semper nulla maximus sodales. Vestibulum ut lectus magna. Nulla
-        facilisi. Sed id elit ut est tincidunt ultricies non a sem. Aenean
-        lobortis, orci vel porttitor pulvinar, nunc ligula consequat est, non
-        pellentesque risus tortor eget ligula. Fusce felis elit, elementum eu
-        ullamcorper quis, tincidunt sed odio. In lacinia sagittis vulputate. Nam
-        vestibulum in metus nec accumsan. Quisque dapibus ac ligula sed
-        convallis. In ligula ligula, semper eget tristique id, vestibulum ut
-        magna. Aenean facilisis lorem non turpis auctor ornare. Vestibulum quis
-        eros sit amet dui commodo pellentesque non vitae nulla. Suspendisse
-        efficitur metus lorem, id volutpat est convallis vitae. Vestibulum ante
-        ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-        Maecenas sem lectus, condimentum ut massa non, imperdiet facilisis
-        turpis. Suspendisse rhoncus mauris sit amet mi cursus, eget tincidunt
-        nisl dapibus. Vivamus lacinia odio ut auctor semper. Cras eget dapibus
-        mi. Curabitur vitae vestibulum diam. Praesent gravida vel nisl bibendum
-        mollis. Fusce at felis suscipit, gravida eros a, lobortis ipsum.
-        Pellentesque sollicitudin, nunc elementum pharetra vulputate, velit
-        ipsum dignissim nunc, ac posuere augue lacus elementum quam. Donec
-        fermentum nulla vehicula dapibus mattis. Suspendisse pharetra urna ac
-        nisi pretium, facilisis accumsan turpis placerat. Mauris ullamcorper eu
-        neque nec venenatis. Suspendisse malesuada est et augue varius
-        scelerisque. Pellentesque ultrices justo ex, ac viverra lorem luctus sit
-        amet.
-      </p>
+      <Grid container className="grid">
+        <Grid item xs={6}>
+          <ProfileSection />
+        </Grid>
+        <Grid item xs={6}>
+          <InfoSection />
+        </Grid>
+      </Grid>
     </div>
   );
 };
