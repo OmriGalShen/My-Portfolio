34336833b3e6e51dbd892e4b1974eda9174728cb
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-10-06 01:07:50 +0300

    refactor link list

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,30 +1,7 @@
 import React from "react";
 import "./Landing.css";
 import avatar from "../../assets/images/myAvatar2.png";
-// import { SocialIcon } from "react-social-icons";
-
-const myLinks = [
-  {
-    name: "email",
-    url: "mailto:shenhav.omri@gmail.com",
-    icon: "fa fa-envelope-o"
-  },
-  {
-    name: "github",
-    url: "https://github.com/OmriGalShen",
-    icon: "fa fa-github"
-  },
-  {
-    name: "linkedin",
-    url: "https://www.linkedin.com/in/omri-gal-shenhav-95235232/",
-    icon: "fa fa-linkedin-square"
-  },
-  {
-    name: "whatsapp",
-    url: "https://wa.me/972543926292",
-    icon: "fa fa-whatsapp"
-  }
-];
+import { linkList } from "./myLinks";
 
 const Landing = props => {
   return (
@@ -35,7 +12,7 @@ const Landing = props => {
         <hr />
         <p className="white f3">Java | Python | Javascript | React | NodeJS</p>
         <div className="social-links">
-          {myLinks.map((link, index) => {
+          {linkList.map((link, index) => {
             return (
               <div key={index} className="grow">
                 <a href={link.url} target="_blank" rel="noopener noreferrer">
---
+++ src/components/landing/myLinks.js
@@ -0,0 +1,22 @@
+export const linkList = [
+  {
+    name: "email",
+    url: "mailto:shenhav.omri@gmail.com",
+    icon: "fa fa-envelope-o"
+  },
+  {
+    name: "github",
+    url: "https://github.com/OmriGalShen",
+    icon: "fa fa-github"
+  },
+  {
+    name: "linkedin",
+    url: "https://www.linkedin.com/in/omri-gal-shenhav-95235232/",
+    icon: "fa fa-linkedin-square"
+  },
+  {
+    name: "whatsapp",
+    url: "https://wa.me/972543926292",
+    icon: "fa fa-whatsapp"
+  }
+];
