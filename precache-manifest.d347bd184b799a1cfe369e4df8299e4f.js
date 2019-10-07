b57cb15f62b79b15c5c21318f9745b4a083a82fc
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-10-08 01:15:31 +0300

    Updates

---
+++ precache-manifest.d347bd184b799a1cfe369e4df8299e4f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c469b0a492e4667d431b30985de5e8ff",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "0e95165b946abc3612cb",
+    "url": "/my-profile/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "06b4d83645c0633dad94",
+    "url": "/my-profile/static/css/main.77ecf95e.chunk.css"
+  },
+  {
+    "revision": "0e95165b946abc3612cb",
+    "url": "/my-profile/static/js/2.a3103386.chunk.js"
+  },
+  {
+    "revision": "06b4d83645c0633dad94",
+    "url": "/my-profile/static/js/main.2e480611.chunk.js"
+  },
+  {
+    "revision": "e0fa47220b8d875affbd",
+    "url": "/my-profile/static/js/runtime-main.595d259b.js"
+  },
+  {
+    "revision": "4f27b5df8993bacd70dc07f9c1d8c943",
+    "url": "/my-profile/static/media/about_bg.4f27b5df.jpeg"
+  },
+  {
+    "revision": "86b3e9841d7ea0db8b4e9dd131e1a87d",
+    "url": "/my-profile/static/media/defaul_project.86b3e984.png"
+  },
+  {
+    "revision": "61ee111725529ff0271667eaa39e2e73",
+    "url": "/my-profile/static/media/myAvatar.61ee1117.png"
+  },
+  {
+    "revision": "61ee111725529ff0271667eaa39e2e73",
+    "url": "/my-profile/static/media/myAvatar2.61ee1117.png"
+  },
+  {
+    "revision": "a35abf5f2212bc6921bac59deb9ab211",
+    "url": "/my-profile/static/media/profile_pic_2.a35abf5f.jpg"
+  },
+  {
+    "revision": "0af3332f0caeebcc65e08cb1fd6c541f",
+    "url": "/my-profile/static/media/projects_bg.0af3332f.jpeg"
+  },
+  {
+    "revision": "a99e488922a9d6bb5d7cafe096c45ee6",
+    "url": "/my-profile/static/media/tech_bg_4.a99e4889.jpg"
+  }
+]);
