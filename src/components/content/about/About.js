47866369287e68b4a2067b247a2adca11ccfdd6b
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-10-10 22:36:14 +0300

    Added content text

--- src/components/content/about/About.js
+++ src/components/content/about/About.js
@@ -18,22 +18,38 @@ const About = props => {
           <h1 className="about-title">About Me</h1>
           <div className="about-description">
             <p>
-              Hello, my name is Omri Gal Shenhav. I live in Kfar Menahem, a
-              kibbutz in sothern Israel. I like to go to the gym and going with
-              friends to the cinema or for a pizza. My family have 3 dogs which
-              I help to take out for a walk on a daily basis.
+              Hello, my name is Omri Gal Shenhav.
+              <br /> I live in Kfar Menahem, a kibbutz in southern Israel.
+              <br /> I like to go to the gym and going with friends to the
+              cinema or for a pizza. My family has 3 dogs which I help to take
+              out for a walk daily.
             </p>
             <p>
-              Hello, my name is Omri Gal Shenhav. I live in Kfar Menahem, a
-              kibbutz in sothern Israel. I like to go to the gym and going with
-              friends to the cinema or for a pizza. My family have 3 dogs which
-              I help to take out for a walk on a daily basis.
+              I was first exposed to programming at 8th grade when my friend
+              showed me a c++ program he wrote, which seemed very cool to me.
+              Together we wrote very simple text games, using tutorials in
+              Youtube.
             </p>
             <p>
-              Hello, my name is Omri Gal Shenhav. I live in Kfar Menahem, a
-              kibbutz in sothern Israel. I like to go to the gym and going with
-              friends to the cinema or for a pizza. My family have 3 dogs which
-              I help to take out for a walk on a daily basis.
+              In 10th grade, I choose to study computer science and physics. In
+              the computer science class, we learned the basics of programming
+              in C#. I also made a simple news site using HTML/CSS/JS and C#
+              with .NET framework. I also participated in a project called
+              "Computer Science, Academia and Industry" by Weizmann Institute of
+              Science, in which I made a simple geometric optics simulator
+              (WinForms application) using C#.
+            </p>
+            <p>
+              In the middle of my mandatory army service, I decided I miss
+              studying and I was motivated to develop myself. To so I started to
+              take Computer Science courses at the Open University. I studies
+              fully independently using mostly books and some videos at the
+              University site.
+            </p>
+            <p>
+              Today I study independently second-year courses but interested in
+              my first job in the field. In the meanwhile, I also study using
+              the internet Python, React, Node.js and more.
             </p>
           </div>
         </div>
