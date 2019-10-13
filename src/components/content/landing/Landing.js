5fe285f1df20340ce2c307db38a22f905876b26c
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-10-13 16:12:29 +0300

    change banner design to bright

--- src/components/content/landing/Landing.js
+++ src/components/content/landing/Landing.js
@@ -6,8 +6,11 @@ import { Paper } from "@material-ui/core";
 
 const myStyles = {
   paper: {
-    background: "rgba(0, 0, 0, 0.9)",
-    color: "white",
+    // background: "rgba(0, 0, 0, 0.9)",
+    // backgroundImage: "linear-gradient(315deg, #bdd4e7 0%, #8693ab 74%)",
+    // backgroundImage: "linear-gradient(315deg, #b8c6db 0%, #f5f7fa 74%)",
+    backgroundImage: "linear-gradient(90deg, #e3ffe7 0%, #d9e7ff 100%)",
+    color: "black",
     border: "3px solid black",
     textAlign: "center",
     display: "flex",
@@ -23,9 +26,9 @@ const Landing = props => {
     <div className="landing">
       <Paper style={myStyles.paper}>
         <img alt="avatar" src={avatar} className="avatar-pic" />
-        <h1 className="f1 white title">Full Stack Developer</h1>
+        <h1 className="f1 title">Full Stack Developer</h1>
         <hr />
-        <p className="white f3">Java | Python | Javascript | React | NodeJS</p>
+        <p className="f3">Java | Python | Javascript | React | NodeJS</p>
         <div className="social-links">
           {linkList.map((link, index) => {
             return (
