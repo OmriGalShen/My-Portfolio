cf805e6c450835bc666ab686b8bed5739002921e
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2019-10-13 15:44:01 +0300

    changed padding and changed landing design

--- src/components/content/landing/Landing.js
+++ src/components/content/landing/Landing.js
@@ -2,11 +2,26 @@ import React from "react";
 import "./Landing.css";
 import avatar from "../../../assets/images/avatar_mini.png";
 import { linkList } from "./myLinks";
+import { Paper } from "@material-ui/core";
+
+const myStyles = {
+  paper: {
+    background: "rgba(0, 0, 0, 0.9)",
+    color: "white",
+    border: "3px solid black",
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    padding: "3em"
+  }
+};
 
 const Landing = props => {
   return (
     <div className="landing">
-      <div className="banner">
+      <Paper style={myStyles.paper}>
         <img alt="avatar" src={avatar} className="avatar-pic" />
         <h1 className="f1 white title">Full Stack Developer</h1>
         <hr />
@@ -22,7 +37,7 @@ const Landing = props => {
             );
           })}
         </div>
-      </div>
+      </Paper>
     </div>
   );
 };
